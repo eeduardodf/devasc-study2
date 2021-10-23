de554e7ba41757c68da3c6a3c6bc086034af71eb
eeduardodf <i201912253@cibertec.edu.pe>
2021-10-23 03:09:51 +0000

    Nuevo archivo

---
+++ info_personal.py
@@ -0,0 +1,12 @@
+#while True :
+name = input("Hola, ¿Cuál es su nombre?\n")
+apellido = input("¿Cuales son sus apellidos?\n")
+edad = str(input("¿Cuantos años tiene? \n"))
+lugar = input("¿Cual es su lugar de residencia?\n")
+telef = str(input("¿Cuál es el número de su teléfono celular o fijo?\n"))
+carrera = input("¿Qué carrera esta estudiando?\n")
+print("Esta es la información que se ha recopilado:\n" + "Su nombre es "+name+" "+apellido+ 
+" tiene, "+edad+" años,"+" vive en "+lugar+
+" , su número de telefono es "+telef+" y estudia la carrera de "+carrera+".")
+
+
